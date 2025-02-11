b61ca22cb7fbe6050091b4c2080d124358a9d041
Benjamin Zinschitz <benjamin@formunauts.at>
2025-02-11 23:50:54 +0100

    get rid of testing setting

--- backend/jamflow/core/config.py
+++ backend/jamflow/core/config.py
@@ -1,38 +1,51 @@
+import importlib
+import os
+
 from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy.pool import Pool
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         # Use top level .env file (one level above ./backend/)
-        env_file="../.env",
+        env_file=os.getenv("ENV_FILE", "../.env"),
         env_ignore_empty=True,
         extra="ignore",
     )
 
-    PROJECT_NAME: str
+    PROJECT_NAME: str = "JamFlow"
 
     DEBUG: bool = False
-    TESTING: bool = False
 
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    DB_POOL: str | None = None
 
     @computed_field
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        db_name = f"{self.DB_NAME}_test" if self.TESTING else self.DB_NAME
         return PostgresDsn.build(
             scheme="postgresql+asyncpg",
             host=self.DB_HOST,
             port=self.DB_PORT,
-            path=db_name,
+            path=self.DB_NAME,
             username=self.DB_USER,
             password=self.DB_PASSWORD,
         )
+
+    @computed_field
+    @property
+    def DB_POOL_CLASS(self) -> Pool | None:
+        if not self.DB_POOL:
+            return None
+
+        pool_module = importlib.import_module("sqlalchemy.pool")
+        pool_class = getattr(pool_module, self.DB_POOL)
+        return pool_class
 
 
 settings = Settings()  # pyright: ignore [reportCallIssue]
--- backend/jamflow/core/db.py
+++ backend/jamflow/core/db.py
@@ -1,17 +1,14 @@
 from collections.abc import AsyncGenerator
 
-from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from jamflow.core.config import settings
-
-pool_args = {"poolclass": NullPool} if settings.TESTING else {}
 
 engine = create_async_engine(
     str(settings.SQLALCHEMY_DATABASE_URI),
     future=True,
     echo=settings.DEBUG,
-    **pool_args,
+    poolclass=settings.DB_POOL_CLASS,
 )
 session_factory = async_sessionmaker(engine, expire_on_commit=False)
 
