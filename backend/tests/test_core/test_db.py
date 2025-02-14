fcaacddac2fedffb20f958a16376896c52b6419f
Benjamin Zinschitz <benjamin@formunauts.at>
2025-02-14 09:59:10 +0100

    test: ensure test isolation through transation rollbacks

--- backend/tests/test_core/test_db.py
+++ backend/tests/test_core/test_db.py
@@ -1,25 +1,35 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import select
-
-from jamflow.models.user import User
+from sqlmodel import text
 
 
+@pytest.mark.dependency()
 @pytest.mark.asyncio
-async def test_create_user(session: AsyncSession):
-    existing_users = (await session.execute(select(User))).scalars().all()
-    assert len(existing_users) == 0
-
-    test_name = "test"
-    session.add(User(username=test_name))
+async def test_rollbacks_between_functions_create_table(session: AsyncSession):
+    # Create the table
+    await session.execute(text("CREATE TABLE isolation_test (id INTEGER PRIMARY KEY)"))
     await session.commit()
 
-    existing_users = (await session.execute(select(User))).scalars().all()
-    assert len(existing_users) == 1
-    assert existing_users[0].username == test_name
+    # Check if the table exists
+    result = await session.execute(
+        text(
+            "SELECT 1 FROM information_schema.tables WHERE table_name = 'isolation_test'"
+        )
+    )
+    table_exists = result.scalar()
+
+    assert table_exists, "Table 'isolation_test' was not created"
 
 
+@pytest.mark.dependency(depens=["test_table_creation"])
 @pytest.mark.asyncio
-async def test_rollbacks_between_functions(session: AsyncSession):
-    existing_users = (await session.execute(select(User))).scalars().all()
-    assert len(existing_users) == 0
+async def test_rollbacks_between_functions_select_table(session: AsyncSession):
+    # Check if the table does not exist
+    result = await session.execute(
+        text(
+            "SELECT 1 FROM information_schema.tables WHERE table_name = 'isolation_test'"
+        )
+    )
+    table_exists = result.scalar()
+
+    assert not table_exists, "Table 'isolation_test' still exists"
