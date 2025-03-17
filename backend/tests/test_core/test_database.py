90a4ffa917a2ee39baa71a0973bbb675157bca88
Benjamin Zinschitz <benjamin@formunauts.at>
2025-03-17 22:50:08 +0100

    feat(backend): get rid of pytest-order

--- backend/tests/test_core/test_database.py
+++ backend/tests/test_core/test_database.py
@@ -5,7 +5,17 @@ from sqlmodel import text
 
 @pytest.mark.integration
 @pytest.mark.asyncio
-async def test_rollbacks_between_functions_create_table(db_session: AsyncSession):
+async def test_rollbacks_between_functions_ping(db_session: AsyncSession):
+    # Check if the table does not exist
+    result = await db_session.execute(
+        text(
+            "SELECT 1 FROM information_schema.tables WHERE table_name = 'isolation_test'"
+        )
+    )
+    table_exists = result.scalar()
+
+    assert not table_exists, "Table 'isolation_test' still exists"
+
     # Create the table
     await db_session.execute(
         text("CREATE TABLE isolation_test (id INTEGER PRIMARY KEY)")
@@ -23,10 +33,9 @@ async def test_rollbacks_between_functions_create_table(db_session: AsyncSession
     assert table_exists, "Table 'isolation_test' was not created"
 
 
-@pytest.mark.order(after=["test_table_creation"])
 @pytest.mark.integration
 @pytest.mark.asyncio
-async def test_rollbacks_between_functions_select_table(db_session: AsyncSession):
+async def test_rollbacks_between_functions_pong(db_session: AsyncSession):
     # Check if the table does not exist
     result = await db_session.execute(
         text(
@@ -36,3 +45,19 @@ async def test_rollbacks_between_functions_select_table(db_session: AsyncSession
     table_exists = result.scalar()
 
     assert not table_exists, "Table 'isolation_test' still exists"
+
+    # Create the table
+    await db_session.execute(
+        text("CREATE TABLE isolation_test (id INTEGER PRIMARY KEY)")
+    )
+    await db_session.commit()
+
+    # Check if the table exists
+    result = await db_session.execute(
+        text(
+            "SELECT 1 FROM information_schema.tables WHERE table_name = 'isolation_test'"
+        )
+    )
+    table_exists = result.scalar()
+
+    assert table_exists, "Table 'isolation_test' was not created"
