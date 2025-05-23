6df6e00117c34cbd797b6cb241ae37987b88b94f
Benjamin Zinschitz <benjamin@formunauts.at>
2025-05-23 16:51:38 +0200

    feat(backend): add clip model

---
+++ backend/jamflow/models/clip.py
@@ -0,0 +1,11 @@
+from pydantic import UUID4
+from sqlmodel import Field
+
+from .base import BaseSQLModel
+
+
+class Clip(BaseSQLModel, table=True):
+    title: str = Field(max_length=255)
+    track_id: UUID4 = Field(foreign_key="track.id", index=True)
+    start: int  # in milliseconds
+    end: int  # in milliseconds
