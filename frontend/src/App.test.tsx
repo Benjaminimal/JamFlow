a3907a8914d53fc9c9690daa163a3a85bb1f7103
Benjamin Zinschitz <benjamin@formunauts.at>
2025-07-23 12:49:47 +0200

    chore(frontend): set up testing

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const paragraphElement = screen.getByText(
+    /click on the vite and react logos to learn more/i,
+  );
+  expect(paragraphElement).toBeInTheDocument();
+});
