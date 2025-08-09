85a1c4fc4aba4997c7d03da965f4b42ea8f55906
Benjamin Zinschitz <benjamin@formunauts.at>
2025-08-09 14:43:57 +0200

    chore(frontend): add vitest linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import unusedImports from "eslint-plugin-unused-imports";
 import { globalIgnores } from "eslint/config";
+import vitest from "@vitest/eslint-plugin";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -14,6 +15,7 @@ export default tseslint.config([
     plugins: {
       "simple-import-sort": simpleImportSort,
       "unused-imports": unusedImports,
+      vitest,
     },
     extends: [
       js.configs.recommended,
@@ -26,6 +28,7 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
+      ...vitest.configs.recommended.rules,
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       // Replace the TypeScript unused vars rule with the plugin version
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+
 import viteconfig from "./vite.config";
 
 export default defineConfig({
