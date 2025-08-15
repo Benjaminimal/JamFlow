d9b0e3f01fd7597cf59bfa0a76900727b0c4dae3
Benjamin Zinschitz <benjamin@formunauts.at>
2025-08-16 00:37:56 +0200

    feat(frontend): implement track page error state

--- frontend/src/pages/TrackList.tsx
+++ frontend/src/pages/TrackList.tsx
@@ -3,13 +3,21 @@ import type { JSX } from "react";
 import { useTrackList } from "@/hooks/useTrackList";
 
 export default function TrackList(): JSX.Element {
-  const { tracks, loading } = useTrackList();
+  const { tracks, loading, errorMessage, fetchData } = useTrackList();
   if (loading)
     return (
       <>
         <p>Loading...</p>
       </>
     );
+  if (errorMessage) {
+    return (
+      <>
+        <p>{errorMessage}</p>
+        <button onClick={fetchData}>Retry</button>
+      </>
+    );
+  }
   if (tracks.length === 0)
     return (
       <>
