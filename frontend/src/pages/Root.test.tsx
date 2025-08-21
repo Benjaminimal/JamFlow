189d29220c095fb2224a8bbf6096c1325373c6b1
Benjamin Zinschitz <benjamin@formunauts.at>
2025-08-21 21:44:30 +0200

    fix(frontend): add missing tracks page link

--- frontend/src/pages/Root.test.tsx
+++ frontend/src/pages/Root.test.tsx
@@ -2,10 +2,18 @@ import { screen } from "@testing-library/react";
 
 import { renderRoute } from "@/test-utils/render";
 
-// TODO: add test for complete and functional navigation
+describe("Root page", () => {
+  beforeEach(() => {
+    renderRoute("/");
+  });
 
-test("render JamFlow header", () => {
-  renderRoute("/");
-  const headerElement = screen.getByText("JamFlow");
-  expect(headerElement).toBeInTheDocument();
+  it("renders the root page", () => {
+    expect(screen.getByText("JamFlow")).toBeInTheDocument();
+  });
+
+  it("renders the navigation links", () => {
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Upload" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Tracks" })).toBeInTheDocument();
+  });
 });
