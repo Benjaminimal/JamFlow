b800e71a3f273823567be2fa20131c1fd4963b7e
Benjamin Zinschitz <benjamin@formunauts.at>
2025-08-29 12:15:55 +0200

    feat(frontend): integrate howler in useAudioPlayer hook

--- frontend/src/contexts/AudioPlayerContext.ts
+++ frontend/src/contexts/AudioPlayerContext.ts
@@ -4,11 +4,11 @@ import { ApplicationError } from "@/errors";
 import type { Playable } from "@/types";
 
 export type AudioPlayerContextType = {
-  load: (v: Playable) => Promise<void>;
+  load: (v: Playable) => void;
 };
 
 export const AudioPlayerContext = createContext<AudioPlayerContextType>({
-  load: async () => {
+  load: () => {
     throw new ApplicationError("load called outside of AudioPlayerProvider");
   },
 });
--- frontend/src/hooks/useAudioPlayer.ts
+++ frontend/src/hooks/useAudioPlayer.ts
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { Howl } from "howler";
+import { useEffect, useRef, useState } from "react";
 
 import type { Playable } from "@/types";
 
 type UseAudioPlayerResult = {
-  load: (playable: Playable) => Promise<void>;
+  load: (playable: Playable) => void;
   title: string;
   active: boolean;
   duration: number;
@@ -25,29 +26,143 @@ export function useAudioPlayer(): UseAudioPlayerResult {
   const [volume, _setVolume] = useState(75);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  const howlRef = useRef<Howl | null>(null);
 
-  const load = async (playable: Playable) => {
+  useEffect(() => {
+    return () => {
+      if (howlRef.current) {
+        howlRef.current.unload();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    let animationFrameCallbackId: number | undefined;
+
+    const syncPosition = () => {
+      const howl = howlRef.current;
+      if (!howl) return;
+
+      if (!howl.playing()) return;
+
+      setPosition(secondsToMs(howl.seek()));
+      animationFrameCallbackId = requestAnimationFrame(syncPosition);
+    };
+
+    if (isPlaying) {
+      animationFrameCallbackId = requestAnimationFrame(syncPosition);
+    }
+
+    return () => {
+      if (animationFrameCallbackId) {
+        cancelAnimationFrame(animationFrameCallbackId);
+      }
+    };
+  }, [isPlaying]);
+
+  const load = (playable: Playable) => {
     setActive(true);
     setTitle(playable.title);
-    setDuration(playable.duration);
-    seek(0);
-    setIsPlaying(true);
+
+    if (howlRef.current) {
+      howlRef.current.unload();
+      howlRef.current = null;
+    }
+
+    howlRef.current = new Howl({
+      src: [playable.url],
+      volume: percentToFactor(volume),
+      mute: isMuted,
+      onload: () => {
+        console.log("Audio onload");
+
+        const _duration = secondsToMs(howlRef.current!.duration());
+        setDuration(_duration);
+
+        setPosition(0);
+
+        howlRef.current!.play();
+      },
+      onloaderror: () => {
+        console.error("Audio onloaderror");
+        // TODO: handle error
+      },
+      onplay: () => {
+        console.log("Audio onplay");
+
+        setIsPlaying(true);
+      },
+      onplayerror: () => {
+        console.error("Audio onplayerror");
+        // TODO: handle error
+      },
+      onend: () => {
+        console.log("Audio onend");
+
+        setIsPlaying(false);
+      },
+      onpause: () => {
+        console.log("Audio onpause");
+
+        setIsPlaying(false);
+      },
+      onstop: () => {
+        console.log("Audio onstop");
+
+        setIsPlaying(false);
+      },
+      onseek: () => {
+        console.log("Audio onseek");
+
+        const _position = howlRef.current!.seek();
+        setPosition(secondsToMs(_position));
+      },
+      onmute: () => {
+        console.log("Audio onmute");
+
+        setIsMuted(howlRef.current!.mute());
+      },
+      onvolume: () => {
+        console.log("Audio onvolume");
+
+        const _volume = howlRef.current!.volume();
+        _setVolume(factorToPercent(_volume));
+      },
+    });
   };
 
   const togglePlay = () => {
-    setIsPlaying(!isPlaying);
+    const howl = howlRef.current;
+    if (!howl) return;
+
+    if (howl.playing()) {
+      howl.pause();
+    } else {
+      howl.play();
+    }
   };
 
   const toggleMute = () => {
-    setIsMuted(!isMuted);
+    const howl = howlRef.current;
+    if (!howl) return;
+
+    howl.mute(!isMuted);
   };
 
   const setVolume = (v: number) => {
-    _setVolume(Math.max(0, Math.min(100, v)));
+    const howl = howlRef.current;
+    if (!howl) return;
+
+    const nextVolume = Math.max(0, Math.min(100, v));
+    howl.volume(percentToFactor(nextVolume));
   };
 
   const seek = (v: number) => {
-    setPosition(Math.max(0, Math.min(duration, v)));
+    const howl = howlRef.current;
+    if (!howl) return;
+
+    const nextPosition = Math.max(0, Math.min(duration, v));
+    howl.seek(msToSeconds(nextPosition));
   };
 
   return {
@@ -64,4 +179,19 @@ export function useAudioPlayer(): UseAudioPlayerResult {
     isMuted,
     toggleMute,
   };
+}
+
+function msToSeconds(ms: number) {
+  return ms / 1000;
+}
+function secondsToMs(seconds: number) {
+  return Math.round(seconds * 1000);
+}
+
+function percentToFactor(percent: number) {
+  return percent / 100;
+}
+
+function factorToPercent(factor: number) {
+  return Math.round(factor * 100);
 }
