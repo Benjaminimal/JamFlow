082361d96a2e1968ba48bcd6be9e24a90fbabfe1
Benjamin Zinschitz <benjamin@formunauts.at>
2025-09-18 20:21:52 +0200

    style(frontend): factor out common patterns for UploadForm

--- frontend/src/components/UploadForm.tsx
+++ frontend/src/components/UploadForm.tsx
@@ -1,4 +1,4 @@
-import { type JSX, useRef } from "react";
+import { type ComponentProps, type JSX, useRef } from "react";
 
 import type { ValidationErrorDetails } from "@/errors";
 import { Button } from "@/ui-lib";
@@ -16,7 +16,6 @@ type UploadFormProps = {
   disabled: boolean;
 };
 
-// TODO: rework errors
 // TODO: add a date picker
 export function UploadForm({
   title,
@@ -42,38 +41,48 @@ export function UploadForm({
       }}
     >
       <div className="my-4 flex flex-col gap-4">
-        {renderErrors(formErrors.nonField)}
-        <div className="flex flex-col gap-1">
-          <Label htmlFor="title">Title</Label>
+        {formErrors.nonField && formErrors.nonField.length > 0 && (
+          <div role="alert" id="form-errors">
+            {formErrors.nonField.map((message, idx) => (
+              <ErrorDisplay key={idx} message={message} />
+            ))}
+          </div>
+        )}
+
+        <FormField id="title" label="Title" errors={formErrors.title}>
           <Input
             id="title"
             type="text"
             value={title}
             onChange={(e) => onTitleChange(e.target.value)}
+            aria-describedby={formErrors.title ? "title-errors" : undefined}
           />
-          {renderErrors(formErrors.title)}
-        </div>
-        <div className="flex flex-col gap-1">
-          <Label htmlFor="recordedDate">Recorded on</Label>
+        </FormField>
+        <FormField
+          id="recordedDate"
+          label="Recorded on"
+          errors={formErrors.recordedDate}
+        >
           <Input
             id="recordedDate"
             type="date"
             value={recordedDate || ""}
             onChange={(e) => onRecordedDateChange(e.target.value)}
             placeholder="Recorded Date"
+            aria-describedby={
+              formErrors.recordedDate ? "recordedDate-errors" : undefined
+            }
           />
-          {renderErrors(formErrors.recordedDate)}
-        </div>
-        <div className="flex flex-col gap-1">
-          <Label htmlFor="file">File</Label>
+        </FormField>
+        <FormField id="file" label="Track" errors={formErrors.file}>
           <Input
             id="file"
             type="file"
             ref={fileInputRef}
             onChange={(e) => onFileChange(e.target.files?.[0] || null)}
+            aria-describedby={formErrors.file ? "file-errors" : undefined}
           />
-          {renderErrors(formErrors.file)}
-        </div>
+        </FormField>
         <div className="flex justify-end">
           <Button type="submit" disabled={disabled} variant="default">
             Upload
@@ -84,18 +93,42 @@ export function UploadForm({
   );
 }
 
-function renderErrors(errors: string[] | undefined): JSX.Element | null {
-  if (!errors?.length) {
-    return null;
-  }
+type FormFieldProps = {
+  id: string;
+  label: string;
+  children: JSX.Element;
+  errors?: string[];
+};
+
+function FormField({
+  id,
+  label,
+  children,
+  errors,
+}: FormFieldProps): JSX.Element {
   return (
-    // TODO: add aria-describedby to form fields
-    <div role="alert">
-      {errors.map((message, idx) => (
-        <p key={idx} className="text-destructive text-sm">
-          {message}
-        </p>
-      ))}
+    <div className="flex flex-col gap-1">
+      <Label htmlFor={id}>{label}</Label>
+      {children}
+      {errors && errors.length > 0 && (
+        <div id={`${id}-errors`}>
+          {errors.map((message, idx) => (
+            <ErrorDisplay key={idx} message={message} />
+          ))}
+        </div>
+      )}
     </div>
+  );
+}
+
+type ErrorDisplayProps = Omit<ComponentProps<"p">, "className"> & {
+  message: string;
+};
+
+function ErrorDisplay({ message, ...props }: ErrorDisplayProps): JSX.Element {
+  return (
+    <p className="text-destructive mt-1 text-sm" {...props}>
+      {message}
+    </p>
   );
 }
