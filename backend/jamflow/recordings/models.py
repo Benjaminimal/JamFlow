139359e2645ea0a877b2ba87ea66049733f7aa2a
Benjamin Zinschitz <b.zinschitz@zinimal.xyz>
2026-04-07 23:36:46 +0200

    ref(models): move Track and Clip into domain

---
+++ backend/jamflow/recordings/models.py
@@ -0,0 +1,30 @@
+from datetime import date
+
+from pydantic import UUID4
+from sqlmodel import Field
+
+from jamflow.core.models import AudioFileFormat, BaseSQLModel, str_enum_to_sa_column
+
+
+class Track(BaseSQLModel, table=True):
+    title: str = Field(max_length=255)
+    duration: int  # in milliseconds
+    format: AudioFileFormat = Field(
+        sa_column=str_enum_to_sa_column(AudioFileFormat, nullable=False)
+    )
+    size: int  # in bytes
+    path: str
+    recorded_date: date | None
+
+
+class Clip(BaseSQLModel, table=True):
+    title: str = Field(max_length=255)
+    track_id: UUID4 = Field(foreign_key="track.id", index=True)
+    duration: int  # in milliseconds
+    start: int  # in milliseconds
+    end: int  # in milliseconds
+    format: AudioFileFormat = Field(
+        sa_column=str_enum_to_sa_column(AudioFileFormat, nullable=False)
+    )
+    size: int  # in bytes
+    path: str
