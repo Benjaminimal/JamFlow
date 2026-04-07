6f3f45d82fa1158d20eb75e971e5a1fe3047b802
Benjamin Zinschitz <b.zinschitz@zinimal.xyz>
2026-04-08 00:26:02 +0200

    ref(validators): move into core

--- backend/jamflow/schemas/validators.py
+++ backend/jamflow/schemas/validators.py
@@ -1,15 +1,6 @@
 # TODO: remove me
+from jamflow.core.validators import empty_string_to_none  # noqa: F401
 from jamflow.recordings.validators import (  # noqa: F401
     get_file_size_validator,
     validate_audo_file_format,
 )
-
-
-def empty_string_to_none(value: str | None) -> str | None:
-    """
-    This validator can be used in the `BeforeValidator` to ensure that
-    empty strings are converted to `None` before validation.
-    A use case could be for ommited or null form fields (multipart/form-data)
-    as FastAPI treats them as empty strings ("") instead of None.
-    """
-    return None if value == "" else value
