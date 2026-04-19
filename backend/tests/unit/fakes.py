d1f639acd50251076d78bbef100b68f5b663143b
Benjamin Zinschitz <b.zinschitz@zinimal.xyz>
2026-04-19 18:48:09 +0200

    feat(tests): add fakes

---
+++ backend/tests/unit/fakes.py
@@ -0,0 +1,103 @@
+import uuid
+from io import BytesIO
+from types import TracebackType
+from typing import BinaryIO, Self, Sequence
+
+from jamflow.core.exceptions import StorageError
+from jamflow.infra.database.models import BaseSQLModel
+from jamflow.recordings.models import AudioFileFormat, Clip, Track
+
+
+class FakeBaseRepository[M: BaseSQLModel]:
+    def __init__(self):
+        self.models: dict[uuid.UUID, M] = {}
+
+    async def create(self, model: M) -> M:
+        self.models[model.id] = model
+        return model
+
+    async def get_by_id(self, id: uuid.UUID) -> M | None:
+        return self.models.get(id, None)
+
+    async def list_all(self) -> Sequence[M]:
+        return list(self.models.values())
+
+    async def list_by_ids(self, ids: list[uuid.UUID]) -> Sequence[M]:
+        return [m for (i, m) in self.models.items() if i in ids]
+
+
+class FakeTrackRepository(FakeBaseRepository[Track]):
+    pass
+
+
+class FakeClipRepository(FakeBaseRepository[Clip]):
+    async def list_by_track_id(self, track_id: uuid.UUID) -> Sequence[Clip]:
+        return [c for c in self.models.values() if c.track_id == track_id]
+
+
+class FakeAudioProcessor:
+    def __init__(
+        self,
+        *,
+        file_format: AudioFileFormat = AudioFileFormat.MP3,
+        duration: int = 42_000,
+        size: int = 1024,
+        clip_bytes: bytes = b"default bytes",
+    ):
+        self.file_format = file_format
+        self.duration = duration
+        self.size = size
+        self.clip_bytes = clip_bytes
+
+    def get_format(self, file: BinaryIO) -> AudioFileFormat:
+        return self.file_format
+
+    def get_duration(self, file: BinaryIO, file_format: AudioFileFormat) -> int:
+        return self.duration
+
+    def get_size(self, file: BinaryIO) -> int:
+        return self.size
+
+    def clip(
+        self, file: BinaryIO, file_format: AudioFileFormat, *, start: int, end: int
+    ) -> BinaryIO:
+        return BytesIO(self.clip_bytes)
+
+
+class FakeAudioStorage:
+    def __init__(self):
+        self.files: dict[str, BinaryIO] = {}
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        pass
+
+    async def store_file(
+        self,
+        file: bytes | BinaryIO,
+        *,
+        path: str,
+        content_type: str,
+    ) -> None:
+        if isinstance(file, bytes):
+            file = BytesIO(file)
+        self.files[path] = file
+
+    async def get_file(self, path: str) -> BinaryIO:
+        if path not in self.files:
+            raise StorageError(f"Unable to retrieve file: Invalid {path=}")
+
+        return self.files[path]
+
+    async def generate_expiring_url(self, path: str, expiration: int = 3600) -> str:
+        if path not in self.files:
+            raise StorageError(f"Unable to generate url for file: Invalid {path=}")
+
+        return f"http://bogus.url/{path}?expiration={expiration}"
