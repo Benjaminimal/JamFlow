29fef701b01ffb02118d112b36310c40b66d719a
Benjamin Zinschitz <b.zinschitz@zinimal.xyz>
2026-04-28 21:14:27 +0200

    refactor(infra): make sqlalchemy enum generator more type safe

--- backend/jamflow/infra/database/models/__init__.py
+++ backend/jamflow/infra/database/models/__init__.py
@@ -1,7 +1,7 @@
 from .base import BaseSQLModel
-from .enums import str_enum_to_sa_column
+from .enums import str_enum_to_sa_enum
 
 __all__ = [
     "BaseSQLModel",
-    "str_enum_to_sa_column",
+    "str_enum_to_sa_enum",
 ]
--- backend/jamflow/infra/database/models/enums.py
+++ backend/jamflow/infra/database/models/enums.py
@@ -1,23 +1,19 @@
 from enum import StrEnum
-from typing import Any
 
-from sqlalchemy import Enum as SQLEnum
-from sqlmodel import Column
+from sqlalchemy import Enum as SAEnum
 
 
-def str_enum_to_sa_column(
-    str_enum: type[StrEnum], **enum_kwargs: Any
-) -> Column[SQLEnum]:
+def str_enum_to_sa_enum(str_enum: type[StrEnum]) -> SAEnum:
     """
-    Convert a StrEnum to a SQLAlchemy Enum column while using the StrEnum values.
+    Convert a `StrEnum` to a `SQLAlchemy` Enum while using the `StrEnum` values.
 
-    This is needed because SQLModel uses the property name.
+    This is needed because `SQLModel` uses the property name which would lead
+    to different values in the database.
     """
-    return Column(
-        SQLEnum(
-            str_enum,
-            name=str_enum.__name__.lower(),
-            values_callable=lambda e: [str(v) for v in e],
-        ),
-        **enum_kwargs,
+    enum_type = SAEnum(
+        str_enum,
+        name=str_enum.__name__.lower(),
+        values_callable=lambda e: [str(v) for v in e],
     )
+
+    return enum_type
